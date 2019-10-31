4b3bdb1a5d95a102e3b9693556bf9c75d79670ba
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-31 08:40:57 -0400

    Setting up

---
+++ react-component-lib/src/components/Button/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Button.css'
+
+// This is a functional component - just sent up a little differently as an arrow function!
+const Button = (props) => {
+    let classList = ""
+    let types = ['primary', 'danger', 'success', 'warning', 'danger-outline']
+    if (types.includes(props.type)) {
+        classList += ` button-${props.type}`
+    }
+    if (props.large) {
+        classList += ' button-large'
+    }
+    return (
+      <button className={classList} onClick={props.onClick}>
+        {props.label}
+      </button>
+    );
+}
+
+export default Button;
