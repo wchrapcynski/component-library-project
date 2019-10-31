875f23432e78e01a7c69562abe71648793ac5143
William Chrapcynski <wchrapcynski@gmail.com>
2019-10-31 16:56:23 -0400

    Moved files

--- src/components/Forms/Forms.stories.js
+++ src/components/Forms/Forms.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from "@storybook/react";
 import Forms from "./Forms";
 
 storiesOf("Forms", module)
-  .add("E-mail", () => <Forms label="Email" type="email" />)
+  .add("E-mail Small-Size", () => <Forms label="Email" type="email" />)
   .add("E-mail Medium-Size", () => <Forms label="Email" type="email" mediumsize />)
   .add("E-mail Large-Size", () => <Forms label="Email" type="email" largesize />);
