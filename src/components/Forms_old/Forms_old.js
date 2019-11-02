957d2c817b078441ed43c8d36dab7624b86964eb
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-02 14:37:37 -0400

    Changing Forms to a class

---
+++ src/components/Forms_old/Forms_old.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "./Forms.css";
+
+// This is a functional component - just sent up a little differently as an arrow function!
+const Forms_old = props => {
+    let classList = "";
+    let classCheckbox = "checkbox-container ";
+    let classCheckmark = "checkmark "
+    let types = [
+        "email",
+        "select",
+        "counter",
+        "textAndutton",
+        "checkbox"
+    ];
+    if (types.includes(props.type)) {
+        classList += ` forms-${props.type}`;
+    }
+    if (props.mediumsize) {
+        classList += " forms-medium-size"
+    }
+    if (props.largesize) {
+      classList += " forms-large-size";
+    }
+    if (props.medselect) {
+      classList += " medselect"
+    }
+    if (props.largeselect) {
+      classList += " largeselect";
+    }
+    if (props.filledselect) {
+      classList += " filled-select";
+    }
+    if (props.checkboxBlue) {
+      classCheckbox += " checkbox-container-blue";
+      classCheckmark += " checkmark-blue";
+    }
+
+    let counterNum = 1;
+    
+  if(props.type === "email") {
+    return (
+      <div className="form">
+        <p className={props.text}>{props.label}</p>
+        <input type={props.label} placeholder={props.label} className={classList}
+        />
+      </div>
+    );
+  } else if (props.type === "select") {
+    return (
+      <div className="form">
+        <select name="selectList" form="selectForm" className={classList}>
+          <option value="select1">{props.selecttext}</option>
+        </select>
+      </div>
+    );
+  } else if (props.type === "counter") {
+    return (
+      <div className="form">
+        <div className={props.type}>
+          <span className="minus">-</span>
+          <span className="number">{counterNum}</span>
+          <span className="plus">+</span>
+        </div>
+      </div>
+    );
+  } else if (props.type === "textAndutton") {
+    return (
+      <div className="form">
+        <form action={props.action}>
+          <input type="text" name={props.data} className={props.submitText} placeholder={props.placeholder}></input>
+          <input type="submit" value={props.value} className={props.submitButton}></input>
+        </form>
+      </div>
+    );
+  } else if (props.type === "checkbox") {
+    return (
+      <div className="form">
+        <label className={classCheckbox}>
+          <input type={props.type} class={props.type} />
+          <span className={classCheckmark}></span>
+          <div>{props.checkboxText}</div>
+        </label>
+      </div>
+    );
+  }
+};
+
+export default Forms_old;
---
+++ src/components/Forms_old/Forms_old.stories.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Forms from "./Forms_old";
+import arrowdown from "./images/arrowdown.svg";
+
+storiesOf("Forms_old", module)
+  .add("E-mail Small-Size", () => <Forms label="Email" type="email" text="text" />)
+  .add("E-mail Medium-Size", () => <Forms label="Email" type="email" text="text" mediumsize />)
+  .add("E-mail Large-Size", () => <Forms label="Email" type="email" text="text" largesize />)
+  .add("Select Small-Size", () => <Forms label="Select" type="select" selecttext="Select" />)
+  .add("Select Medium-Size", () => <Forms label="Select" type="select" selecttext="Select" medselect/>)
+  .add("Select Large-Size", () => <Forms label="Select" type="select" selecttext="Select" largeselect/>)
+  .add("Select Filled Small-Size", () => <Forms label="Select" type="select" selecttext="Select" filledselect/>)
+  .add("Select Filled Medium-Size", () => <Forms label="Select" type="select" selecttext="Select" medselect filledselect/>)
+  .add("Select Filled Large-Size", () => <Forms label="Select" type="select" selecttext="Select" largeselect filledselect/>)
+  .add("Counter", () => <Forms label="Counter" type="counter"/>)
+  .add("Text and Button Small", () => 
+  <Forms label="textAndutton" type="textAndutton" action="http://www.bing.com/?" 
+    data="search" submitButton="submit-button" submitText="submit-text" value="Redeem" placeholder="Voucher code"
+  />)
+  .add("Text and Button Large", () => 
+  <Forms label="textAndutton" type="textAndutton" action="http://www.bing.com/?" 
+    data="search" submitButton="submit-button-large" submitText="submit-text-large" value="Redeem" placeholder="Voucher code"
+    />)
+  .add("Checkbox Black", () => <Forms label="CheckBox" type="checkbox" checkboxText="Text" />)
+  .add("Checkbox Blue", () => <Forms label="CheckBox" type="checkbox" checkboxText="Text" checkboxBlue />);
