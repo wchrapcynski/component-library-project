bff608a8980bcc8ef79b545d6b598a14ca9eb90e
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-02 14:47:19 -0400

    Forms now working as a class. Old function version moved to root

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -34,11 +34,55 @@ class Forms extends Component {
             this.classCheckbox += " checkbox-container-blue";
             this.classCheckmark += " checkmark-blue";
         }
-        return (
-            <div className="Form">
-                test
-            </div>
-        );
+
+        let counterNum = 1;
+
+        if (this.props.type === "email") {
+            return (
+                <div className="form">
+                    <p className={this.props.text}>{this.props.label}</p>
+                    <input type={this.props.label} placeholder={this.props.label} className={this.classList}
+                    />
+                </div>
+            );
+        } else if (this.props.type === "select") {
+            return (
+                <div className="form">
+                    <select name="selectList" form="selectForm" className={this.classList}>
+                        <option value="select1">{this.props.selecttext}</option>
+                    </select>
+                </div>
+            );
+        } else if (this.props.type === "counter") {
+            return (
+                <div className="form">
+                    <div className={this.props.type}>
+                        <span className="minus">-</span>
+                        <span className="number">{counterNum}</span>
+                        <span className="plus">+</span>
+                    </div>
+                </div>
+            );
+        } else if (this.props.type === "textAndutton") {
+            return (
+                <div className="form">
+                    <form action={this.props.action}>
+                        <input type="text" name={this.props.data} className={this.props.submitText} placeholder={this.props.placeholder}></input>
+                        <input type="submit" value={this.props.value} className={this.props.submitButton}></input>
+                    </form>
+                </div>
+            );
+        } else if (this.props.type === "checkbox") {
+            return (
+                <div className="form">
+                    <label className={this.classCheckbox}>
+                        <input type={this.props.type} class={this.props.type} />
+                        <span className={this.classCheckmark}></span>
+                        <div>{this.props.checkboxText}</div>
+                    </label>
+                </div>
+            );
+        }
     }
 }
 
