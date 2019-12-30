aab27ce7d84fc1c9e4a0f8d69949ad92f46cd000
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-30 18:46:59 -0500

    Removing mutating of state on thumbnails

--- src/components/Thumbnails/Thumbnails.js
+++ src/components/Thumbnails/Thumbnails.js
@@ -6,7 +6,7 @@ class Thumbnails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blockClassV: "thumbnail-container-v ", blockClassH: "thumbnail-container-h ", inlineClass: "thumbnail-container-inline",
+      blockClassH: "thumbnail-container-h ", inlineClass: "thumbnail-container-inline",
       descClass: "thumbnail-desc ",
       stars: parseInt(props.stars), starImages: ["star-gray", "star-gray", "star-gray", "star-gray", "star-gray"],
       thumbImage: props.image, salePrice: props.salePrice, retailPrice: props.retailPrice, heartImage: props.heartImage,
@@ -26,40 +26,53 @@ class Thumbnails extends Component {
       }
     } 
 
-    // Adds the correct classes for changing colors of background and horiziontal line
-    if (this.props.backgroundFilled) {
-      this.state.blockClassV += "thumbnail-filled "
-      this.state.descClass += "thumbnail-desc-dark "
-    }
-
     // Vertical Blocks colors, icons, and images are set by props in the story
     if (this.props.type === "blockv") {
-      return(
-          <div className="thumbnails">
-              <div className={this.state.blockClassV}>
-                  <div className="thumbnail-img"><img src={require(`./images/${this.state.thumbImage}.png`)} width="236" /></div>
-                  <div className="buttons">
-                    <img src={require(`./images/${this.state.heartImage}.svg`)} />
-                    <span className="space"></span>
-                    <img src={require(`./images/${this.state.cartImage}.svg`)} />
-                  </div>
-                  <div className="thumbnail-bottom">
-                    <div className={this.state.descClass}>{this.state.thumbtext}</div>
-                    <div className="rating">
-                      <div className={this.state.starImages[0]}></div>
-                      <div className={this.state.starImages[1]}></div>
-                      <div className={this.state.starImages[2]}></div>
-                      <div className={this.state.starImages[3]}></div>
-                      <div className={this.state.starImages[4]}></div>
-                    </div>
-                    <div className="price">
-                      <span className="sale">${this.state.salePrice}</span>
-                      <span className="space"></span>
-                      <span className="retail">${this.state.retailPrice}</span>
-                    </div>
-                  </div>
+      return (
+        <div className="thumbnails">
+          <div
+            className={
+              this.props.backgroundFilled
+                ? "thumbnail-filled thumbnail-container-v"
+                : "thumbnail-container-v"
+            }
+          >
+            <div className="thumbnail-img">
+              <img
+                src={require(`./images/${this.state.thumbImage}.png`)}
+                width="236"
+              />
+            </div>
+            <div className="buttons">
+              <img src={require(`./images/${this.state.heartImage}.svg`)} />
+              <span className="space"></span>
+              <img src={require(`./images/${this.state.cartImage}.svg`)} />
+            </div>
+            <div className="thumbnail-bottom">
+              <div
+                className={
+                  this.props.backgroundFilled
+                    ? "thumbnail-desc thumbnail-desc-dark"
+                    : "thumbnail-desc"
+                }
+              >
+                {this.state.thumbtext}
               </div>
+              <div className="rating">
+                <div className={this.state.starImages[0]}></div>
+                <div className={this.state.starImages[1]}></div>
+                <div className={this.state.starImages[2]}></div>
+                <div className={this.state.starImages[3]}></div>
+                <div className={this.state.starImages[4]}></div>
+              </div>
+              <div className="price">
+                <span className="sale">${this.state.salePrice}</span>
+                <span className="space"></span>
+                <span className="retail">${this.state.retailPrice}</span>
+              </div>
+            </div>
           </div>
+        </div>
       );
     }
     // Horizontal Version
